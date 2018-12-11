5ea8d92cb88961730cdfad0f348c9fc8c87bc4da
Kazuma Matsudo <matsudo.kazuma@unipro.co.jp>
2018-12-11 19:50:47 +0900

    Update App.js

--- App.js
+++ App.js
@@ -7,23 +7,38 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+import { MobileAppBridge } from 'NativeModules';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+async function displayHelloWorld (self) {
+  try {
+    const text = await MobileAppBridge.sayHelloWorld("Android");
+
+    self.setState({
+      hello: text
+    })
+  } catch (e) {
+    console.error(e)
+  }
+}
 
 type Props = {};
 export default class App extends Component<Props> {
+  constructor(props){
+    super(props);
+    this.state = {};
+  }
+
+  componentDidMount() {
+    displayHelloWorld(this);
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <Text style={styles.welcome}>
+          rust says: {this.state.hello}
+        </Text>
       </View>
     );
   }
@@ -40,10 +55,5 @@ const styles = StyleSheet.create({
     fontSize: 20,
     textAlign: 'center',
     margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
   },
 });
